f6b0bd9bcdf3f8c1e8911fe98a09512b3d8b036b
Kyeongjun Ko <95596243+kyeongjun-ko@users.noreply.github.com>
2024-04-21 03:00:44 -0700

    Time: 891 ms (5.20%), Space: 61.6 MB (5.12%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,38 +3,17 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    const highest = Math.max(...prices);
-    const lowest = Math.min(...prices);
-
-    
-    // if (prices.indexOf(lowest) === prices.length - 1 || highest === lowest) {
-    //     return 0;
-    // }
-    
-    const lowerIndex = prices.indexOf(lowest);
-    
-    const rest = prices.slice(lowerIndex + 1);
-    const highestOfRest = Math.max(...rest);
-    
-    // return highestOfRest - lowest;
-    
     const copyPrices = [...prices];
     
     let profit = 0;
     let length = copyPrices.length;
     let principal;
-    
-    
-
-           
      
     for (let i = 0; i < length - 1; i++) {
         const target = copyPrices[i];
-        
 
         if (principal === undefined || target < principal) {
             principal = target;
-            console.log("principal", principal);
             
             if (i + 1 < copyPrices.length) {
                 copyPrices.splice(0, i + 1);                
@@ -43,15 +22,10 @@ var maxProfit = function(prices) {
         
             length = copyPrices.length;
             
-            console.log("length",length, "i", i)
-            console.log("copyPrices", copyPrices);
-
             const highestValue = Math.max(...copyPrices);
-            console.log("highestValue", highestValue);
             
             if (principal < highestValue && highestValue - principal > profit) {
                 profit = highestValue - principal;
-                console.log("profit", profit, "length", length );
             }
             
             if (length > 1) {
