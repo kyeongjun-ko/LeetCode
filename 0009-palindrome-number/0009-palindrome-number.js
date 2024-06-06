9aa194c85cb83a0539bab3b580ed65fd8f61219b
Kyeongjun Ko <95596243+kyeongjun-ko@users.noreply.github.com>
2024-06-06 13:15:35 +0900

    Time: 189 ms (5.10%), Space: 58.6 MB (28.24%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x < 0) {
+        return false;
+    }
+    
+    const stringNumber = [...String(x)];
+    
+    while (stringNumber.length > 1) {
+        const front = stringNumber.shift();
+        const back = stringNumber.pop();
+        
+        if (front !== back) {
+            return false;
+        }
+    }
+    
+    // return true;
+    console.log("stringNumber", stringNumber);
+    
+    if (stringNumber.length === 1 || stringNumber.length === 0) {
+        return true;
+    }
+};
