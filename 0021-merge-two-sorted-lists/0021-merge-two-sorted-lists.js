7e32db0a6bed45ddadfa349071bc1bbdd4988234
Kyeongjun Ko <95596243+kyeongjun-ko@users.noreply.github.com>
2024-08-29 17:16:54 +0900

    Time: 71 ms (15.11%), Space: 52.6 MB (5.27%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+const addList = (list, value) => {
+    if (list === null) {
+        if (typeof value === "number") {
+            return new ListNode(value);
+        } else {
+            return value;
+        }
+    }
+    
+    list.next = addList(list.next, value);
+    
+    return list;
+};
+
+
+var mergeTwoLists = function(list1, list2) {
+    if (list1 === null || list2 === null) {
+        return list1 || list2;
+    };
+    
+    let  mergedList = null;
+    
+    let listA = new ListNode(list1.val, list1.next);
+    let listB = new ListNode(list2.val, list2.next);
+    
+    while (listA !== null && listB !== null) {
+        if (listA && listB) {
+            if (listA.val >= listB.val) {
+                mergedList = addList(mergedList, listB.val);
+
+                listB = listB.next;
+            } else {
+                mergedList = addList(mergedList, listA.val);
+                
+                listA = listA.next;
+            }
+        }
+    };
+    
+
+    if (listA === null) {
+        mergedList = addList(mergedList, listB);
+    } else {
+        mergedList = addList(mergedList, listA);
+    }
+    
+    return mergedList;
+};
